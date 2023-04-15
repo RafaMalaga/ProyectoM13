250c28849d0385f46194db3eac89fe8ce46d3116
Rafael Aguilar <101289667+RafaMalaga@users.noreply.github.com>
2023-04-16 00:19:28 +0200

    he eliminado la barra superior de color morado donde salia el nombre de la app

--- app/src/main/java/com/example/proyectom13/MainActivity.java
+++ app/src/main/java/com/example/proyectom13/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.proyectom13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     
