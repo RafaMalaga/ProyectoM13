8c62516ad9db86e8a8f15dbd31eb61fb4045e825
Rafael Aguilar <101289667+RafaMalaga@users.noreply.github.com>
2023-04-16 22:37:26 +0200

    unir codigo rama principal (#19)
    * diseño pantalla inicio con botón de registro y enter
    
    * Codigo login
    
    * Creado formulario de alta y botón para elegir idioma entre inglés y español
    
    * Revert "Merge pull request #12 from RafaMalaga/master"
    
    * Codigo login
    button redondo
    
    ---------
    
    Co-authored-by: idurrea <127035696+idurrea@users.noreply.github.com>
    Co-authored-by: antikiko <89138263+antikiko@users.noreply.github.com>

---
+++ app/src/main/java/com/example/proyectom13/RegistroActivity.java
@@ -0,0 +1,12 @@
+package com.example.proyectom13;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistroActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
