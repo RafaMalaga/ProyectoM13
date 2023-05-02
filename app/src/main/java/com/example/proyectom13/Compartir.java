a431f3574475d486486867294a7c87dd934781e7
idurrea <127035696+idurrea@users.noreply.github.com>
2023-05-03 00:47:32 +0200

    se puede compartir info de los objetos via email, pendiente seleccionar username y detectar email

--- app/src/main/java/com/example/proyectom13/Compartir.java
+++ app/src/main/java/com/example/proyectom13/Compartir.java
@@ -6,12 +6,16 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -29,9 +33,9 @@ import java.util.ArrayList;
 public class Compartir extends AppCompatActivity {
 
 
-
     private ListView lstResultados;
     private ArrayList<String> resultados;
+    ArrayList<String> itemSeleccionados;
 
     @SuppressLint("StaticFieldLeak")
     @Override
@@ -53,56 +57,41 @@ public class Compartir extends AppCompatActivity {
         });
 
         resultados = new ArrayList<>();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, resultados);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_multichoice, resultados);
         lstResultados.setAdapter(adapter);
 
-        btnCompartir.setOnClickListener(new View.OnClickListener() {
+        
+        int selectedItem = 0;
+        lstResultados.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                ArrayList<String> itemSeleccionados = new ArrayList<>();
 
-                share();
+                // Agregar los elementos seleccionados al ArrayList
+                SparseBooleanArray checkedItems = lstResultados.getCheckedItemPositions();
+                for (int i = 0; i < checkedItems.size(); i++) {
+                    if (checkedItems.valueAt(i)) {
+                        itemSeleccionados.add(resultados.get(checkedItems.keyAt(i)));
+
+                    }
+                    btnCompartir.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            share(itemSeleccionados);
+                        }
+                    });
+                }
+
+
             }
+
+
+
         });
 
-        // Obtener los últimos 5 objetos alamcenados en  la base de datos siempre que se abre la acitvity
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    String url = "http://" + MainActivity.HOST + "/api/ultimos_objetos.php?cantidad=5";
-                    HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.connect();
-                    int responseCode = connection.getResponseCode();
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        InputStream inputStream = connection.getInputStream();
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                        StringBuilder stringBuilder = new StringBuilder();
-                        String line;
-                        while ((line = bufferedReader.readLine()) != null) {
-                            stringBuilder.append(line);
-                        }
-                        Log.d("RESPONSE", stringBuilder.toString());
-                        JSONArray jsonArray = new JSONArray(stringBuilder.toString());
-                        for (int i = 0; i < jsonArray.length(); i++) {
-                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            String nombre = jsonObject.getString("nombre"); // en la vista "previa" solo obtenemos nombre y descripcion
-                            String descripcion = jsonObject.getString("descripcion");
-                            resultados.add(nombre + ": " + descripcion);
-                        }
-                    }
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                adapter.notifyDataSetChanged();
-            }
-        }.execute();
 
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("StaticFieldLeak")
@@ -158,12 +147,17 @@ public class Compartir extends AppCompatActivity {
         });
     }
 
-    private void share() {
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.setType("text/plain");
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "Este es mi texto para compartir.");
-        startActivity(Intent.createChooser(sendIntent, "Compartir"));
+    private void share(ArrayList itemSeleccionados) {
+        String subject = "Asunto del correo";
+        String encabezado = "Este es el encabezado del mensaje que se comparte:\n\n";
+        String mensaje = encabezado + TextUtils.join("\n", itemSeleccionados);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, mensaje);
+        // Para compartir mediante email, puedes agregar el destinatario en el Intent
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"destinatario@example.com"});
+        startActivity(Intent.createChooser(intent, "Compartir con"));
     }
 }
 
